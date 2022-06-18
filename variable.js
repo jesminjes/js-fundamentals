2b9062cefa08ced99dbbd20185f1f099774c5481
jesminjes <mstjesmina56@gmail.com>
2022-06-18 23:17:49 +0600

    true false

---
+++ variable.js
@@ -0,0 +1,13 @@
+// Number
+var watchprice = 120;
+watchprice = 125;
+
+//  string
+var userName = 'Jesmin Akter';
+
+// Boolean
+var isexpensibe = true;
+
+// Integer:1 2 3 4 5 6 7 124
+// float : 2.63 3.747 3.789
+

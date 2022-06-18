2f895f0ecf0ad7370b5cd85f9c1711b8bc8e7244
jesminjes <mstjesmina56@gmail.com>
2022-06-18 23:40:32 +0600

    length

---
+++ array.js
@@ -0,0 +1,12 @@
+var friendAge = [11, 12, 24,];
+
+var friendName = ['rinku', 'sorno', 'nilu', 'mim'];
+
+var oddNumber = [1, 3, 5, 7, 9];
+
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+console.log(vowels);
+
+var vowelsCount = vowels.length;
+console.log(vowelsCount);

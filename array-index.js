e517f6a6ce67daef5f137ad4e269d22c6ecd5e02
jesminjes <mstjesmina56@gmail.com>
2022-06-21 23:09:00 +0600

    indexof

---
+++ array-index.js
@@ -0,0 +1,9 @@
+var bookPages = [ 'hablu', 'bolod', 'battery', 'peracitamol'];
+
+var batteryIndex = bookPages.indexOf('battery');
+console.log(batteryIndex);
+
+
+var numbers = [ 23, 45, 78, 77, 98 ];
+var index = numbers.indexOf(77);
+console.log(index);

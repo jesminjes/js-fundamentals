6f8bd95616410025188353497ca241ad90fc151f
jesminjes <mstjesmina56@gmail.com>
2022-06-21 23:17:16 +0600

    numbers

--- array-index.js
+++ array-index.js
@@ -1,9 +1,17 @@
 var bookPages = [ 'hablu', 'bolod', 'battery', 'peracitamol'];
 
 var batteryIndex = bookPages.indexOf('battery');
-console.log(batteryIndex);
+// console.log(batteryIndex);
 
 
 var numbers = [ 23, 45, 78, 77, 98 ];
 var index = numbers.indexOf(77);
-console.log(index);
+// console.log(index);
+
+var books = [ 'hablu', 'bolod', 'battery', 'peracitamol'];
+
+var batteryIndex = books.indexOf('battery');
+console.log(batteryIndex);
+
+ var secondIndex = books[2];
+ console.log(secondIndex);

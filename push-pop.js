534016bcb55b2d8efa65a917a24908f47a5654ce
jesminjes <mstjesmina56@gmail.com>
2022-07-02 02:02:53 +0600

    push-pop

---
+++ push-pop.js
@@ -0,0 +1,13 @@
+// add elements to array
+var lastbench = ['kalam', 'balam', 'salam'];
+lastbench.push('jalam');
+lastbench.push('palam');
+// console.log(lastbench);
+
+var friendsAge = [11, 13, 17, 12];
+fiendsAge.push(15);
+// console.log(friendsAge);
+
+// remove an elements from an array
+friendsAge.pop();
+console.log(friendsAge);

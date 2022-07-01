987039cc3919c7d2d869c11c319dd9c0f2e2636a
jesminjes <mstjesmina56@gmail.com>
2022-07-02 02:24:56 +0600

    and or

---
+++ comparision.js
@@ -0,0 +1,13 @@
+// console.log(5 > 6);
+// console.log(5 < 6);
+// console.log(5 == 6);
+// console.log(6 == 6);
+// console.log(6 != 6);
+// console.log(myLove > yourLove);
+// console.log(myLove == yourLove);
+// console.log(myLove < yourLove);
+// console.log(myLove != yourLove);
+
+
+console.log(studyGood && goodPerson);
+console.log(isBanker || goodResult);

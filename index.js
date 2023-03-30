efb9f32dcca91c50fbfef5f6b714876e9c08398b
jesminjes <mstjesmina56@gmail.com>
2023-03-30 22:02:20 +0600

    indexOf

---
+++ index.js
@@ -0,0 +1,14 @@
+var numbers = [65, 54, 74, 74, 43];
+// console.log(numbers);
+
+// get element value by index
+
+var element = (numbers[2]);
+// console.log(element);
+
+// set element value by index
+numbers[2] = 200;
+// console.log(numbers);
+
+var positionIndex = numbers.indexOf(74);
+console.log(positionIndex);

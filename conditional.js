b7da0031610b7cdcadd728c20977d5e1ed3ed8b2
jesminjes <mstjesmina56@gmail.com>
2023-04-01 22:59:30 +0600

    if-else

--- conditional.js
+++ conditional.js
@@ -1,1 +1,14 @@
-var isFoodReady =  true;
+var iphonePrice = 79000;
+var myBudget = 95000;
+
+
+
+if(iphonePrice < myBudget){
+    console.log('i phone kine');
+}
+
+var chikenPrice = 500;
+var myMoney = 50;
+if(chikenPrice > myMoney){
+    console.log('smashed potato and lentils soup');
+}

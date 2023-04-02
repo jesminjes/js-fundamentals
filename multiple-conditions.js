529c83d3facd0f7fc065b1ae6427db69cd8578b0
jesminjes <mstjesmina56@gmail.com>
2023-04-02 22:19:11 +0600

    if-else

---
+++ multiple-conditions.js
@@ -0,0 +1,17 @@
+var isGraduated = true;
+var salary =2500;
+
+
+// if(isGraduated == true){
+//     console.log('cholo biye kre feli')
+// }
+// else{
+//     console.log('tr kopale biya nai')
+// }
+
+if(isGraduated == true && salary >50000){
+    console.log('eso biye kre feli')
+}
+else{
+    console.log('tr kopale biya nai')
+}
